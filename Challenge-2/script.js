ef6875884bd18e17bc476e4125ad59b1ac7d6eb4
amrgharz <ammor.ghr@gmail.com>
2021-03-06 11:34:15 +0400

    coding-challenge-2

--- Challenge-2/script.js
+++ Challenge-2/script.js
@@ -10,3 +10,10 @@ let markBMI = markMass / (markHieght ** 2);
 let markHigherBMI = markBMI > johnBMI
 
 console.log(johnBMI, markBMI, markHigherBMI)
+
+//Print out to the console whos BMI is higher
+if(markHigherBMI){
+    console.log(`Mark's BMI is ${markBMI} and it is higher than ${johnBMI} Jhon's BMI`)
+}else{
+    console.log(`Jhon's BMI is ${johnBMI} and it is higher than ${markBMI} Mark's BMI`)
+}
